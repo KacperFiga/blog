4a943b7b23914443e9cdc00adc71c396d3dc6b30
KacperFiga <kacper.figa.com@o2.pl>
2022-12-19 20:01:43 +0100

    create template for one post page

--- frontend/pages/post/[id].tsx
+++ frontend/pages/post/[id].tsx
@@ -1,8 +1,43 @@
 import React from "react";
+import axios from "axios";
+import { Box, Typography } from "@mui/material";
+import { POSTS_API } from "../../constants/paths";
+import { postI } from "../../constants/types";
 
-const PostPage = () => {
-  console.log();
-  return <div>Post page</div>;
+const PostPage = ({ data: { data } }: any) => {
+  console.log(data);
+  return (
+    <Box>
+      <Typography>{data.attributes.title}</Typography>
+      <Typography>{data.attributes.content}</Typography>
+      <Typography>{data.attributes.date}</Typography>
+    </Box>
+  );
+};
+
+export const getStaticProps = async ({ params }: postI) => {
+  const response = await axios.get(`${POSTS_API}/${params.id}`);
+
+  return {
+    props: {
+      data: response.data,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const postsResponse = await axios.get(POSTS_API);
+
+  const paths = postsResponse.data.data.map((post: postI) => ({
+    params: {
+      id: post.id?.toString(),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
 };
 
 export default PostPage;
