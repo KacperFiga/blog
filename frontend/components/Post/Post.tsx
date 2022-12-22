f2a90c26cb25dcf42af8eaaab77ef62d19489829
KacperFiga <kacper.figa.com@o2.pl>
2022-12-22 18:12:42 +0100

    create main wall

--- frontend/components/Post/Post.tsx
+++ frontend/components/Post/Post.tsx
@@ -1,17 +1,44 @@
 import { Box, Typography } from "@mui/material";
 import Link from "next/link";
+import { shades } from "../../constants/theme";
+import styles from "./Post.module.css";
+
+const boxStyles = {
+  backgroundColor: shades.neutral[300],
+  padding: "20px",
+  borderRadius: "20px",
+  color: shades.primary[500],
+};
+
+const descriptionBoxStyles = {
+  fontSize: "0.8em",
+};
+
+const articleHeaderStyles = {
+  marginBottom: "20px",
+};
+
+const headerTextStyles = {
+  fontWeight: "600",
+};
+const articleDateStyles = {
+  marginTop: "15px",
+  fontSize: "0.8em",
+};
 
 export const Post = ({ title, description, date, id }: any) => (
-  <Link href={`/post/${id}`}>
-    <Box>
-      <header>
-        <h3> {title}</h3>
-      </header>
+  <Link href={`/post/${id}`} className={styles.container}>
+    <Box sx={boxStyles}>
+      <Box sx={articleHeaderStyles}>
+        <Typography color={shades.secondary[500]} sx={headerTextStyles}>
+          {title}
+        </Typography>
+      </Box>
       <article>
-        <Typography>{description}</Typography>
+        <Typography sx={descriptionBoxStyles}>{description}</Typography>
       </article>
       <section>
-        <p>{date}</p>
+        <Typography sx={articleDateStyles}>{date}</Typography>
       </section>
     </Box>
   </Link>
--- frontend/components/Posts/Posts.tsx
+++ frontend/components/Posts/Posts.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Post } from "../Post/Post";
 import { postI, postsI } from "../../constants/types";
+import { Box } from "@mui/material";
+import { shades } from "../../constants/theme";
+
+const boxStyles = {
+  backgroundColor: shades.neutral[100],
+  padding: "40px 10px",
+  borderRadius: "20px",
+};
 
 export const Posts = ({ data }: postsI) => {
   const posts = data.map((post: postI) => (
@@ -13,5 +21,5 @@ export const Posts = ({ data }: postsI) => {
     />
   ));
 
-  return <section>{posts}</section>;
+  return <Box sx={boxStyles}>{posts}</Box>;
 };
